cbfd0cd6207d34424c6dfcf2fe029e15bc45e5cc
bruneez <apollisbruneez@gmail.com>
2025-09-01 14:59:26 +0200

    updated login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -4,7 +4,18 @@ import LoginComponent from "../components/LoginComponent.vue";
 
 <template>
   <div class="d-flex flex-column align-items-center mt-5">
-    <h2 class="mb-4 fw-bold text-center">Login</h2>
+    <!-- Title -->
+    <h2 class="mb-5 fw-bold text-center">Login</h2>
+
+    <!-- Login Form -->
     <LoginComponent />
   </div>
 </template>
+
+<style scoped>
+/* Push title a bit higher so there's space */
+h2 {
+  margin-top: 40px;
+}
+</style>
+
